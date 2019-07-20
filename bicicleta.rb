327e00e28d8a4c14ff6d20ed7ee4e35cf60e99ab
creyes-dev <creyes.dev@gmail.com>
2019-07-20 02:34:22 -0300

    bicicleta montania y carretera

--- bicicleta.rb
+++ bicicleta.rb
@@ -1,28 +1,37 @@
 class Bicicleta
-  attr_reader :tamanio, :color_cinta
+  attr_reader :estilo, :tamanio, :color_cinta,
+              :amortiguador_delantero, :amortiguador_trasero
 
   def initialize(**opts)
+    @estilo = opts[:estilo]
     @tamanio = opts[:tamanio]
     @color_cinta = opts[:color_cinta]
+    @amortiguador_delantero = opts[:amortiguador_delantero]
+    @amortiguador_trasero = opts[:amortiguador_trasero]
   end
 
   # Cada bicicleta tiene el mismo tamaño por defecto del neumático y de la cadena
   def repuestos
-    { cadena:   '11-velocidad',
-      tamanio_neumatico: '23',
-      color_cinta: color_cinta
-    }
+    if estilo == :carretera
+      { cadena:   '11-velocidad',
+        tamanio_neumatico: '23', # milímetros
+        color_cinta: color_cinta
+      }
+    else
+      { cadena: '11-velocidad',
+        tamanio_neumatico: '2.1', # pulgadas
+        color_cinta: amortiguador_delantero
+      }
   end
 
   # otros métodos
 end
 
 bici = Bicicleta.new(
+  estilo: :montania,
   tamanio: 'M',
-  color_cinta: 'rojo'
+  amortiguador_delantero: 'Manitou',
+  amortiguador_trasero: 'Fox'
 )
 
-puts bici.tamanio
-
 puts bicicleta.repuestos
-
